bfc9684ecbc6d79f1afbf4d8ca896567cb07982b
Stephenson-alvaro <novanime.data.github@gmail.com>
2025-03-28 04:13:48 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,11 +13,18 @@ while True:
     url = f"{BASE_URL}{page}"
     print(f"Scraping page {page}...")
     headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Referer": "https://www.google.com/",
+    "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "DNT": "1",
+    "Connection": "keep-alive"
 }
 
 
-    response = requests.get(url, headers=headers)
+
+    response = requests.get(url, headers=headers, allow_redirects=True)
     if response.status_code != 200:
         print(response.status_code)
         print(f"Fin du scraping à la page {page - 1}.")
